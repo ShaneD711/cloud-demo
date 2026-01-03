4e61870f744399cfa10a6fda97125d748f73fe24
shaned <aibuaiwoa@gmail.com>
2026-01-03 19:46:16 +0800

    test(Mybatis-plus): 自定义SQL

--- src/main/java/mp/mapper/UserMapper.java
+++ src/main/java/mp/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import mp.domain.po.User;
 import org.apache.ibatis.annotations.Param;
 
@@ -8,6 +10,19 @@ import java.util.List;
 
 public interface UserMapper extends BaseMapper<User> {
 
+    /**
+     * 测试根据id列表查询用户
+     * Mybatis 在UserMapper.xml书写SQL语句
+     * @param ids
+     * @return
+     */
     List<User> queryUserByIds(@Param("ids") List<Long> ids);
 
+    /**
+     *
+     * @param wrapper
+     * @param amount
+     */
+    void updateBalanceByIds(@Param(Constants.WRAPPER) QueryWrapper<User> wrapper, @Param("amount") int amount);
 }
+
--- src/test/java/mp/mapper/UserMapperTest.java
+++ src/test/java/mp/mapper/UserMapperTest.java
@@ -56,7 +56,7 @@ class UserMapperTest {
 
     /**
      * 测试根据id列表查询用户
-     * 根据自定义SQL语句查询
+     * Mybatis 在UserMapper.xml书写SQL语句
      */
     @Test
     void testQueryByIds2() {
@@ -93,10 +93,7 @@ class UserMapperTest {
     @Test
     void testqueryWrapper() {
         // 构建查询条件
-        QueryWrapper<User> wrapper = new QueryWrapper<User>()
-                .select("id", "username", "info", "balance")
-                .like("username", "Lucy")
-                .ge("balance", 100);
+        QueryWrapper<User> wrapper = new QueryWrapper<User>().select("id", "username", "info", "balance").like("username", "Lucy").ge("balance", 100);
         // 执行查询
         List<User> users = userMapper.selectList(wrapper);
         users.forEach(System.out::println);
@@ -114,8 +111,7 @@ class UserMapperTest {
         User user = new User();
         user.setBalance(2000);
         // 更新的条件
-        QueryWrapper<User> wrapper = new QueryWrapper<User>()
-                .eq("username", "jack");
+        QueryWrapper<User> wrapper = new QueryWrapper<User>().eq("username", "jack");
         // 执行更新
         userMapper.update(user, wrapper);
     }
@@ -130,9 +126,7 @@ class UserMapperTest {
     @Test
     void testUpdateWrapper() {
         List<Long> ids = List.of(1L, 2L, 3L, 4L);
-        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
-                .setSql("balance = balance - 100")
-                .in("id", ids);
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>().setSql("balance = balance - 100").in("id", ids);
         userMapper.update(null, wrapper);
     }
 
@@ -144,13 +138,34 @@ class UserMapperTest {
     @Test
     void testLambdaQueryWrapper() {
         // 构建查询条件
-        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
-                .select(User::getId, User::getUsername, User::getInfo, User::getBalance)
-                .like(User::getUsername,"o")
-                .ge(User::getBalance,100);
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>().select(User::getId, User::getUsername, User::getInfo, User::getBalance).like(User::getUsername, "o").ge(User::getBalance, 100);
         // 执行查询
         List<User> users = userMapper.selectList(wrapper);
         users.forEach(System.out::println);
+    }
+
+    /**
+     * MP 自定义SQL
+     * MP 自定义 SQL = 自己手写的 SELECT 语句 + MP 自动生成的 WHERE 子句
+     * 步骤:
+     * 1.基于Wrapper构建where条件 调用mapper中的方法
+     * 2.在mapper中的方法 参数用Param注解声明wrapper变量名称,必须为we
+     * 3.在方法上方或者XML中自定义SQL 并使用Wrapper条件
+     *
+     * <p>
+     * 更新id为1,2,4的用户的余额,扣200
+     * UPDATE user SET balance = balance -200 WHERE id in (1,2,4)
+     */
+    @Test
+    void testCustomSqlUpdate() {
+        // 更新条件
+        List<Long> ids = List.of(1L, 2L, 3L, 4L);
+        int amount = 200;
+        // 定义条件
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .in("id", ids);
+        // 调用自定义方法
+        userMapper.updateBalanceByIds(wrapper, amount);
     }
 
 
